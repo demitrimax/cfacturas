04fb2a4f594de8c82c4be8dd7def0c683b64fb39
Moises Aguilar <armandoaguilar1@yahoo.com>
2018-10-25 11:38:40 -0500

    modificaciones al footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,16 +3,16 @@
     <div class="row align-items-center justify-content-xl-between">
       <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-          &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+          &copy; 2018 <a href="#" class="font-weight-bold ml-1" target="_blank">Control de Facturas</a>
         </div>
       </div>
       <div class="col-xl-6">
         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
           <li class="nav-item">
-            <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+            <a href="#" class="nav-link" target="_blank">Veritas Software</a>
           </li>
           <li class="nav-item">
-            <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+            <a href="#" class="nav-link" target="_blank">Reporte un problema</a>
           </li>
           <li class="nav-item">
             <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
